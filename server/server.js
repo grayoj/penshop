79c8bb68c67157d05e00b4e6f952506b6c2fbcd1
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-10-05 14:13:31 +0100

    Push Penshop

---
+++ server/server.js
@@ -0,0 +1,11 @@
+// This will import the express module
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("API is Up and Running!");
+});
+
+app.listen(5000, console.log("Our Server is Up!"));
+
