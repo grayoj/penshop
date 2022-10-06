07cc15d52681153dc6adb93f8c4804f9fef21d1a
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-10-06 16:41:59 +0100

    Updates.

---
+++ client/src/components/Newsletter.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Send } from "@material-ui/icons";
+import styled from "styled-components";
+
+const
+
+
+
+const Newsletter = () => {
+  return (
+    <Container>
+        <Title></Title>
+        <Description></Description>
+        <InputContainer>
+            <Input />
+            <Button>
+                <Send />
+            </Button> 
+        </InputContainer>
+    </Container>
+  )
+}
+
+export default Newsletter;
