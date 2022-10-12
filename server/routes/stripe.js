53431e57fbbdbcea2d78846f80fb0e4b1ca93c75
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-10-12 13:05:16 +0100

    Stripe payment and orderRoutes

---
+++ server/routes/stripe.js
@@ -0,0 +1,20 @@
+// Stripe API Method
+const router = require("express").Router();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+router.post("/payment", (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "naira",
+    },
+    (stripeError, stripeResponse) => {
+      if (stripeError) {
+        res.status(500).json(stripeError);
+      } else {
+        res.status(200).json(stripeResponse);
+      }
+    }
+  );
+});
